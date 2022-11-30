ed2121e64182a3f825439b6c6344a8e630e09406
ahanfairil24 <110127482+ahanfairil24@users.noreply.github.com>
2022-11-30 10:43:52 +0700

    Add files via upload

---
+++ web/about/models.py
@@ -0,0 +1,8 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+class pusink(models.Model):
+    nama = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama
